d359ee1baf69f997522ed1f31252bb40ae146070
Eric Yim <erichyim@gmail.com>
2020-07-04 15:28:15 -0700

    7420

--- projects/admin.py
+++ projects/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 
 from .models import Project
 
-admin.site.register(Project)
+
+class ProjectAdmin(admin.ModelAdmin):
+    #fields = ['url_str','pub_date', 'description']
+    fieldsets = [
+        ('Back',               {'fields': ['url_str','pub_date']}),
+        ('Front',               {'fields': ['description']})
+    ]
+
+admin.site.register(Project,ProjectAdmin)
+
+#admin.site.register(Project)
