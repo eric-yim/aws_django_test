7aeee3afe81dd346db54398696479618305d9ff5
Eric Yim <erichyim@gmail.com>
2020-07-04 12:00:57 -0700

    test

--- projects/views.py
+++ projects/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
     return HttpResponse("Welcome to my <i>App</i>")
+def detail(request, project_id):
+    return HttpResponse("You're looking at question %s.".format(project_id))
