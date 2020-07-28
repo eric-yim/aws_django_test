e7a9d1080c108cb915c388b0c0b6387d875d4d98
EricYim <erichyim@gmail.com>
2020-07-28 09:05:16 -0700

    728

--- aws_django/settings.py
+++ aws_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['yimml.com','127.0.0.1','aws-django-env.eba-pbdbyipb.us-east-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['yimml.com','127.0.0.1','18.188.211.95','3.128.129.186']
 
 
 # Application definition
